33d6e35ac41089b2cdc0cc21e5c18f8ca26e7de1
denisrostov <86770748+denisrostov@users.noreply.github.com>
2021-07-03 17:03:37 +0300

    Upload File

--- src/test/java/guru/qa/FormTests.java
+++ src/test/java/guru/qa/FormTests.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-//import static com.codeborne.selenide.Selenide.*;
-//import static com.codeborne.selenide.Condition.*;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -22,20 +23,38 @@ public class FormTests {
     }
 
     @Test
-    void fillForm () {
+    void fillForm() {
 
 
-        open( "/automation-practice-form");
-       $("#firstName").setValue("Shiva");
+        open("/automation-practice-form");
+        $("#firstName").setValue("Shiva");
 
-       $("#lastName").setValue("Ivanova");
-       $(("[for=\"gender-radio-3\"]")).click();
-//        $(By.xpath("//label[contains(text(),'Male')]")).click();
+        $("#lastName").setValue("Ivanova");
+        $(("#genterWrapper")).click();
+        $(("#hobbies-checkbox-2")).parent().click();
+
+        $(("#dateOfBirthInput")).click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1984");
+        $((".react-datepicker__day--023")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/img/qa.png"));
         $("#userNumber").setValue("1234567890");
-       $("#submit").scrollTo();
-       $("#submit").click();
+        $("#currentAddress").setValue("World");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
 
-       $("#example-modal-sizes-title-lg").shouldHave(text ("Thanks for submitting the form"));
+        $("#submit").scrollTo();
+        $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(
+                text("Shiva Ivanova"),
+                text("23 January,1984"),
+                text("1234567890")
+        );
+
 
     }
 
