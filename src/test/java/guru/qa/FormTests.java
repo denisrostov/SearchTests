c297434f0f77cf1d2832cbb2999ce8c92a3ca7f3
denisrostov <86770748+denisrostov@users.noreply.github.com>
2021-07-26 21:24:45 +0300

    Add ObjectPages

--- src/test/java/guru/qa/FormTests.java
+++ src/test/java/guru/qa/FormTests.java
@@ -3,13 +3,14 @@ package guru.qa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 public class FormTests {
 
     @BeforeAll
@@ -17,30 +18,40 @@ public class FormTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
     }
+
+    String firstName = "Shiva",
+            lastName = "Ivanova",
+            gender = "Other",
+            hobbie = "Reading",
+            month = "January",
+            year = "1984",
+            day = "23",
+            subjects = "comp",
+            picturePath = "src/test/resources/qa.png",
+            mobile = "1234567890",
+            currentAddress = "World",
+            state = "NCR",
+            city = "Delhi";
+
     @Test
     void fillForm() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Shiva");
-        $("#lastName").setValue("Ivanova");
-        $(("#genterWrapper")).$(byText("Other")).click();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $(("#genterWrapper")).$(byText(gender)).click();
+        $("#hobbiesWrapper").$(byText(hobbie)).click();
+        $("#submit").scrollTo();
         $(("#dateOfBirthInput")).click();
-        $(".react-datepicker__month-select").selectOption("January");
-        $(".react-datepicker__year-select").selectOption("1984");
-        $((".react-datepicker__day--023")).click();
-//        альтеонативный выбор даты в календаре
-//        $("#dateOfBirthInput").click();
-//        $("#dateOfBirthInput").sendKeys(Keys.CONTROL, "a");
-//        $("#dateOfBirthInput").sendKeys(Keys.SPACE);
-//        $("#dateOfBirthInput").setValue("23 January 1984").pressEnter();
-        $("#subjectsInput").setValue("comp").pressEnter();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/qa.png"));
-        $("#userNumber").setValue("1234567890");
-        $("#currentAddress").setValue("World");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(String.format(".react-datepicker__day--0%s",day)).click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#uploadPicture").uploadFile(new File(picturePath));
+        $("#userNumber").setValue(mobile);
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").scrollTo().click();
-
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(
